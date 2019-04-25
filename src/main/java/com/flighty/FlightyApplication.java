1f923bcd1b17407a3b013cec08685df8fdd06b1f
greenfir <m18797997521_1@163.com>
2019-04-25 17:41:17 +0800

    Initial commit

---
+++ src/main/java/com/flighty/FlightyApplication.java
@@ -0,0 +1,13 @@
+package com.flighty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlightyApplication.class, args);
+    }
+
+}
